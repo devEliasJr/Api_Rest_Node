d4fd5cbe308896393b422217ad9e13e12c7a8cb8
devEliasJr <eliasengmecanica@gmail.com>
2023-08-14 11:45:48 -0300

    chore: Configure database with Ts

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 declare module "knex/types/tables" {
   interface Transactions {
-    id: number;
+    id: string;
     title: string;
     amount: number;
     created_at: string;
