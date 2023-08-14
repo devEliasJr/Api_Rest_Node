0d3ce8dcd764edc3215e0692aaf36e6f59ae96e5
devEliasJr <eliasengmecanica@gmail.com>
2023-08-14 14:23:29 -0300

    feat: Identification and permissions for users

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,32 +2,72 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { knex } from "../database";
 import { randomUUID } from "node:crypto";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const transactions = await knex("transactions").select();
+  app.addHook('preHandler', async(request, reply) =>{
+    console.log(`[${request.method}, ${request.url}]`)
+  })
 
-    return { transactions };
-  });
+  app.get(
+    "/",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies;
 
-  app.get("/:id", async (request) => {
-    const getTransactionParamsSchema = z.object({
-      id: z.string().uuid(),
-    });
-    const { id } = getTransactionParamsSchema.parse(request.params);
+      const transactions = await knex("transactions")
+        .where("session_id", sessionId)
+        .select();
 
-    const transactions = await knex("transactions").where("id", id).first();
+      return { transactions };
+    }
+  );
 
-    return { transactions };
-  });
+  app.get(
+    "/:id",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const getTransactionParamsSchema = z.object({
+        id: z.string().uuid(),
+      });
+      const { id } = getTransactionParamsSchema.parse(request.params);
 
-  app.get("/summary", async () => {
-    const summary = await knex("transactions")
-      .sum("amount", { as: "amount" })
-      .first();
+      const { sessionId } = request.cookies;
 
-    return { summary };
-  });
+      const transactions = await knex("transactions")
+        .where({
+          session_id: sessionId,
+          id: id,
+        })
+        .first();
+
+      return { transactions };
+    }
+  );
+
+  app.get(
+    "/summary",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+
+    async (request) => {
+      const { sessionId } = request.cookies;
+
+      const summary = await knex("transactions")
+        .where(
+          "session_id", sessionId
+        )
+        .sum("amount", { as: "amount" })
+        .first();
+
+      return { summary };
+    }
+  );
 
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
@@ -40,21 +80,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     );
 
-    let sessionId = request.cookies.sessionID
-    if (!sessionId) {
-      sessionId = randomUUID()
+    let sessionId = request.cookies.sessionId;
 
-      reply.cookie("sessionID", sessionId, {
-        path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-      })
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
     }
 
     await knex("transactions").insert({
       id: randomUUID(),
       title,
       amount: type === "credit" ? amount : amount * -1,
-      session_id:sessionId
+      session_id: sessionId,
     });
 
     // HTTP - CODE: 201  - Criado com sucesso
